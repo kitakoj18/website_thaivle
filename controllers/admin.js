f6754a8d92e866da36174f0ec780dcbc27794917
kitakoj18 <kitakoj18@gmail.com>
2020-08-09 12:24:25 -0700

    added Blog schema and edited postBlog middleware to create new Blog object and save to database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const Blog = require('../models/blog');
+
 const dotenv = require('dotenv').config();
 const tinyAPI = process.env.TINYMCE_API;
 
@@ -9,6 +11,13 @@ exports.postBlog = (req, res, next) =>{
 
     const blogTitle = req.body.title;
     const blogContent = req.body.blogContent;
+
+    const blogPost = new Blog({
+        blogTitle: blogTitle,
+        blogContent: blogContent
+    })
+
+    blogPost.save();
 
     res.redirect('/admin');
 }
---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    blogTitle: {
+        type: String,
+        required: true
+    },
+    blogContent: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
