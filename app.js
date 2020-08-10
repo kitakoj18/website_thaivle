8bca169ad942a1ee89059a6e22ff0a1a004cc788
kitakoj18 <kitakoj18@gmail.com>
2020-08-10 15:07:54 -0700

    fixed /image middleware

--- app.js
+++ app.js
@@ -39,10 +39,12 @@ const fileFilter = (req, file, cb) =>{
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // argument in .single() is the same name given to the file input tag
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('thumbnail'));
+app.use(
+    multer({storage: fileStorage, fileFilter: fileFilter}).single('thumbnail')
+);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('/images', path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/admin', adminRoutes);
 app.use(mainRoutes);
