c597d1fa2343bfae867dffaf2dc08e7ff3abc99a
kitakoj18 <kitakoj18@gmail.com>
2020-08-10 14:27:47 -0700

    finished implementing multer and middleware to store file and filepath

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const path = require('path');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const mongoose = require('mongoose');
 
@@ -14,8 +15,34 @@ const mainRoutes = require('./routes/main');
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false}));
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) =>{
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) =>{
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ){
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// argument in .single() is the same name given to the file input tag
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('thumbnail'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('/images', path.join(__dirname, 'images')));
 
 app.use('/admin', adminRoutes);
 app.use(mainRoutes);
