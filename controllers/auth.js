cd132e893a8bda4a0d45bbad1b8f483a7e3cb46e
kitakoj18 <kitakoj18@gmail.com>
2020-09-20 21:40:24 -0700

    added logic to check if password is correct

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const userEmail = process.env.USER_EMAIL;
+const userPW = process.env.USER_PW;
 
 exports.getLogin = (req, res, next) =>{
     res.render('auth/login')
@@ -8,7 +9,7 @@ exports.getLogin = (req, res, next) =>{
 exports.login = (req, res, next) =>{
     const email = req.body.email;
     const password = req.body.password;
-    if(email !== userEmail){
+    if(email !== userEmail && password !== userPW){
         return res.redirect('/login');
     }
 
