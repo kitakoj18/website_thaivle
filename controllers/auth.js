39fad0620538fe4b4c3f3bde47f394585bf7469e
kitakoj18 <kitakoj18@gmail.com>
2020-09-20 21:38:10 -0700

    added logic for user to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,20 @@
+const dotenv = require('dotenv').config();
+const userEmail = process.env.USER_EMAIL;
+
 exports.getLogin = (req, res, next) =>{
     res.render('auth/login')
 }
 
 exports.login = (req, res, next) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    if(email !== userEmail){
+        return res.redirect('/login');
+    }
 
+    req.session.isLoggedIn = true;
+    req.session.save(err =>{
+        console.log(err);
+        res.redirect('/');
+    })
 }
