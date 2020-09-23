c802ee40c0994f2dfadfa98f6bffad05250a9911
kitakoj18 <kitakoj18@gmail.com>
2020-09-22 21:28:16 -0700

    added route to edit blog

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,8 @@ router.get('/add-blog', isAuth, adminController.addBlog);
 
 router.post('/post-blog', isAuth, adminController.postBlog);
 
+router.get('/edit-blog/:blogId', isAuth, adminController.getEditBlog);
+
 router.post('/delete-blog', isAuth, adminController.deleteBlog);
 
 module.exports = router;
