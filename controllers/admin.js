e8de89de6cf3a96e6fd7cbd910af0cdc529d4b4c
kitakoj18 <kitakoj18@gmail.com>
2020-09-22 21:28:39 -0700

    added controller to edit blog

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv').config();
 const tinyAPI = process.env.TINYMCE_API;
 
 exports.addBlog = (req, res, next) =>{
-    res.render('admin/addBlog', {tinyAPI: tinyAPI});
+    res.render('admin/addBlog', {
+        tinyAPI: tinyAPI,
+        pageTitle: 'Write a blog post below!'
+    });
 };
 
 exports.postBlog = (req, res, next) =>{
-
     const blogTitle = req.body.title;
     // add check if blogThumbnail exists since can be rejected if not right file type
     // added / to beginning to make absolute path
@@ -24,6 +26,28 @@ exports.postBlog = (req, res, next) =>{
     blogPost.save();
 
     res.redirect('/thoughts');
+};
+
+exports.getEditBlog = (req, res, next) =>{
+    const isEditing = req.query.edit;
+    if(!isEditing){
+        return res.redirect('/thoughts');
+    }
+
+    const blogId = req.params.blogId;
+    Blog.findById(blogId)
+        .then(blog =>{
+            if(!blog){
+                return res.redirect('/thoughts');
+            }
+            res.render('admin/addBlog', {
+                tinyAPI: tinyAPI,
+                pageTitle: 'Edit your blog below!',
+                isEditing: isEditing,
+                blog: blog
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.deleteBlog = (req, res, next) =>{
